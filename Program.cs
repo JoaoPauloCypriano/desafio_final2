741d88f2854c3fc59ddd04d03b7ad20979bb782e
JoaoPauloCypriano <jpaulosilvao99@gmail.com>
2026-03-30 14:57:48 -0300

    Desafio_final_2 concluido JP

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string[] pocoes = { "Cura", "Mana" };
+
+        // Verifique se pocoes.Length > 0
+        // Se sim: Console.WriteLine("Poção consumida!");
+        // Senão: Console.WriteLine("Inventário Vazio!");
+        if (pocoes.Length > 0)
+        {
+            Console.WriteLine("Poção consumida!");
+        }
+        else
+        {
+            Console.WriteLine("Iventário Vazio!");
+        }
+
+        int nivelJogador = 55;
+
+        // Se nivelJogador >= 50: "Porta VIP liberada!"
+        // Senão: "Vá para treinamento!"
+        if(nivelJogador>=50)
+        {
+            Console.WriteLine("Porta VIP liberada!");
+        }
+        else
+        {
+            Console.WriteLine("Vá para treinamento!");
+        }
+
+        int noAtual = 50;
+        int valorProcurado = 85;
+
+        // Compare valorProcurado com noAtual
+        // Maior: "Ir para DIREITA"
+        // Menor: "Ir para ESQUERDA"
+        if(valorProcurado>noAtual)
+        {
+            Console.WriteLine("Ir para DIREITA");
+        }
+        else
+        {
+            Console.WriteLine("Ir para ESQUERDA");
+        }     
+    }
+}
